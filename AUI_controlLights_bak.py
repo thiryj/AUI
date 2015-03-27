573fe9987579bdcd63a76303be4e364e5716ffe3
John Thiry <john@thiry.com>
2015-03-26 20:56:27 -0400

    Add control of lights

--- AUI_controlLights_bak.py
+++ AUI_controlLights_bak.py
@@ -11,18 +11,18 @@ import time
 from bs4 import BeautifulSoup
 
 #set local constants
-C4SOAP_NAME = "\"SendToDeviceAsync\""
-ASYNC_STATE = "\"1\""
+C4SOAP_NAME = "SendToDeviceAsync"
+ASYNC_STATE = "1"
 TCP_IP = '192.168.1.111'
 TCP_PORT = 5020
 BUFFER_SIZE = 8192
 ENCODING = "utf-8"
 MESSAGE_TERMINATION = "\0"
-PARAM_NAME_DATA = "\"data\""
-PARAM_NAME_DATA_TYPE = "\"STRING\""
-PARAM_NAME_DEVICE = "\"idDevice\""
-PARAM_NAME_DEVICE_TYPE = "\"INT\""
-COMMAND_SETLEVEL = "SET_LEVEL"
+PARAM_NAME_DATA = "data"
+PARAM_NAME_DATA_TYPE = "STRING"
+PARAM_NAME_DEVIVE = "idDevice"
+PARAM_NAME_DEVIVE_TYPE = "INT"
+COMMAND_SETLEVEL = "SET LEVEL"
 
 '''
 Sets intensity of dimmer switch
@@ -34,24 +34,11 @@ def setLevel(id, value):
 
     directorConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     directorConn.connect((TCP_IP,TCP_PORT))
-    Message_Header_Open = "<c4soap name=" + C4SOAP_NAME + " async=" + ASYNC_STATE + ">"
-    Param_1_Open = "<param name=" + PARAM_NAME_DATA + " type=" + PARAM_NAME_DATA_TYPE + ">"
-    Command_1_Open = "<devicecommand><command>" + COMMAND_SETLEVEL + "</command>"
-    Param_1_2_Set_Open = "<params>"
-    Param_1_2_Set_1_Open = "<param><name>LEVEL</name><value type=" + str(PARAM_NAME_DEVICE_TYPE) + "><static>" + str(value) + "</static></value>"
-    Param_1_2_Set_1_Close = "</param>"
-    Param_1_2_Set_Close = "</params>"
-    Command_1_Close = "</devicecommand>"
-    Param_1_Close = "</param>"
-    Param_2_1_Open = "<param name=" + PARAM_NAME_DEVICE + " type=" + PARAM_NAME_DEVICE_TYPE + ">" + str(id)
-    Param_2_1_Close = "</param>"
-    Message_Header_Close = "</c4soap>"
-    Message = Message_Header_Open + Param_1_Open + Command_1_Open + Param_1_2_Set_Open + Param_1_2_Set_1_Open + Param_1_2_Set_1_Close + \
-              Param_1_2_Set_Close + Command_1_Close + Param_1_Close + Param_2_1_Open + Param_2_1_Close + Message_Header_Close
-    print(Message)
-    Messageb = bytes(Message,ENCODING)
-    directorConn.sendall(Messageb + bytes(MESSAGE_TERMINATION,ENCODING))
+    MESSAGE = '<c4soap name="SendToDeviceAsync" async="1"><param name="data" type="STRING"><devicecommand><command>SET_LEVEL</command><params><param><name>LEVEL</name><value type="INT"><static>%d</static></value></param></params></devicecommand></param><param name="idDevice" type="INT">%d</param></c4soap>' % (value, id)
+    MESSAGEb = bytes(MESSAGE,ENCODING)
+    directorConn.sendall(MESSAGEb + bytes(MESSAGE_TERMINATION,ENCODING))
     directorConn.close()
+    print(MESSAGE)
 
 '''
 Ramps to a specified level in milliseconds
